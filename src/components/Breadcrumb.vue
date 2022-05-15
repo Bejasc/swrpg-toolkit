3226c4b087b3b05eb9dc345cae8b97e64b7baa12
Ben <benjamin.schulz94@gmail.com>
2022-05-16 00:29:03 +0930

    breadcrumb event

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="text-h6">{{ $route.name }}</div>
+	<v-breadcrumbs :items="breadcrumbs"></v-breadcrumbs>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface BreadcrumbData {
+	text: string;
+	disabled: boolean;
+	to: { name: string };
+	exact: boolean;
+}
+
+export default defineComponent({
+	name: "Breadcrumb",
+	computed: {
+		breadcrumbs() {
+			const x = this.$router.getRoutes();
+			x.map((e) => console.log("aa", e));
+
+			//TODO on router event on before route, push next target into it. When going backwards, see if that route already exists in the stack, if so, remove all after it.
+			const data: BreadcrumbData[] = [{ text: "Contributor Tools", disabled: false, to: { name: "Contributor Tools" }, exact: true }];
+			return data;
+		},
+	},
+});
+</script>
--- src/views/Contributor Tools/ContributorDirectory.vue
+++ src/views/Contributor Tools/ContributorDirectory.vue
@@ -2,6 +2,7 @@
 	<ContributorSidebar />
 	<v-main>
 		<v-container fluid>
+			<Breadcrumb />
 			<!-- If using vue-router -->
 			<router-view class="ma-4" />
 		</v-container>
@@ -10,12 +11,13 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Breadcrumb from "../../components/Breadcrumb.vue";
 import ContributorSidebar from "../../components/Contributor Tools/ContributorSidebar.vue";
 
 // Components
 
 export default defineComponent({
 	name: "ContributorDirectory",
-	components: { ContributorSidebar },
+	components: { ContributorSidebar, Breadcrumb },
 });
 </script>
