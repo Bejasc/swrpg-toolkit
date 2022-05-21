fa8e26c947de3ee2c88bcd9ac67bde6e90ce9a4e
Ben <benjamin.schulz94@gmail.com>
2022-05-21 13:56:56 +0930

    add build step

---
+++ src/scripts/gh-pages-deploy.mjs
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+import { execa } from "execa";
+import fs from "fs";
+(async () => {
+	try {
+		await execa("git", ["checkout", "--orphan", "gh-pages"]);
+		// eslint-disable-next-line no-console
+		console.log("Building started...");
+		await execa("npm", ["run", "build"]);
+		// Understand if it's dist or build folder
+		const folderName = fs.existsSync("dist") ? "dist" : "build";
+		await execa("git", ["--work-tree", folderName, "add", "--all"]);
+		await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+		console.log("Pushing to gh-pages...");
+		await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+		await execa("rimraf", ["-r", folderName]);
+		await execa("git", ["checkout", "-f", "master"]);
+		await execa("git", ["branch", "-D", "gh-pages"]);
+		console.log("Successfully deployed, check your settings");
+	} catch (e) {
+		console.log(e.message);
+		console.log(`Something went wrong trying to deploy`);
+		process.exit(1);
+	}
+})();
