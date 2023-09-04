74b438b608dbaab4dae30796476d1a7b13a60890
Bejasc <benjamin.schulz94@gmail.com>
2023-09-04 23:44:10 +0930

    Added google tag manager

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import EventEditor from "@/components/Contributor Tools/Events/EventEditor.vue";
 import EventLink from "@/components/Contributor Tools/Events/EventLink.vue";
 import { createApp } from "vue";
+import VueGtag from "vue-gtag";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
@@ -10,7 +11,11 @@ import { stateStore } from "./stores/MainState";
 loadFonts();
 
 const app = createApp(App);
-app.use(router).use(vuetify).use(stateStore).mount("#app");
+app.use(router)
+	.use(vuetify)
+	.use(stateStore)
+	.use(VueGtag, { config: { id: "G-N7NC3E9LTN" } }, router)
+	.mount("#app");
 stateStore.commit("setShowLoader", false);
 app.component("EventLinkComponent", EventLink);
 app.component("EventEditorComponent", EventEditor);
